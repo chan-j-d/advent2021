12c336cbc56ebe4a4d64aa26de7ef2b44778c4a3
chan-j-d <chan.j.d.98@gmail.com>
2021-12-16 14:35:14 +0800

    Complete day1

--- codes/Day1_2.java
+++ codes/Day1_2.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class Day1 {
+public class Day1_2 {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new FileReader(args[0]));
@@ -10,14 +10,18 @@ public class Day1 {
     }
 
     static int countIncreases(Scanner scanner) {
-        int currentDepth = Integer.MAX_VALUE;
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+        int value3 = scanner.nextInt();
         int count = 0;
         while (scanner.hasNextInt()) {
             int readValue = scanner.nextInt();
-            if (readValue > currentDepth) {
+            if (readValue > value1) {
                 count++;
             }
-            currentDepth = readValue;
+            value1 = value2;
+            value2 = value3;
+            value3 = readValue;
         }
         return count;
     }
